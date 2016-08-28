ba78d003d3a68a658fcf55a8f465caa526d7e078
michzio <michzio@hotmail.com>
2016-08-28 12:50:50 +0200

    added test directory

---
+++ test/test_hash_map.c
@@ -0,0 +1,5 @@
+//
+// Created by Michal Ziobro on 28/08/2016.
+//
+
+#include "test_hash_map.h"
---
+++ test/test_hash_map.h
@@ -0,0 +1,8 @@
+//
+// Created by Michal Ziobro on 28/08/2016.
+//
+
+#ifndef COLLECTIONS_TEST_HASH_MAP_H
+#define COLLECTIONS_TEST_HASH_MAP_H
+
+#endif //COLLECTIONS_TEST_HASH_MAP_H
